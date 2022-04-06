f30c69cdacea2270ec67a1979f0cb739bdbfe23f
cicerofonsecaosg <cicero@osg.ca>
2022-04-06 13:59:09 -0400

    Install OwlUI and update ts configs

--- apps/electron/src/index.tsx
+++ apps/electron/src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Default as Button } from '@owlui/button';
+import { Default as TextField } from '@owlui/textfield';
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
+      <Button appearance="Primary">OwlUI Button</Button>
+      <TextField appearance="Primary"></TextField>
       <h1>Hello from React!</h1>
     </div>
   );
