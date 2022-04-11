c1fd15dfc154464dc56c71170643950ae887dfc3
cicerofonsecaosg <cicero@osg.ca>
2022-04-11 14:20:04 -0400

    [sc-565] Create and style new TitleBar component

--- apps/course-authoring/src/renderer/components/app/index.tsx
+++ apps/course-authoring/src/renderer/components/app/index.tsx
@@ -1,38 +1,41 @@
-import React from 'react'
+import React from 'react';
 import {
   MemoryRouter as Router,
   Routes,
   Route,
   Navigate,
-} from 'react-router-dom'
-import pageHome from '../../pages/home'
+} from 'react-router-dom';
+import pageHome from '../../pages/home';
+import * as styles from './styles/styles.module.scss';
+import { TitleBar } from '../titlebar';
 
-const basename = '/'
-const pages = [pageHome]
+const basename = '/';
+const pages = [pageHome];
 
 const createAppRoutes = () => {
   return pages.map((page, index) => {
     return (
       <Route key={index} path={`${page.Route}`} element={<page.Element />} />
-    )
-  })
-}
+    );
+  });
+};
 
 export const App = () => {
-  const AppRoutes = createAppRoutes()
+  const AppRoutes = createAppRoutes();
 
   return (
     <Router>
-      <main>
+      <TitleBar pages={pages} />
+      <main className={styles.main}>
         <Routes>
           {AppRoutes}
           <Route path="*" element={<Navigate to={basename} />} />
         </Routes>
       </main>
     </Router>
-  )
-}
+  );
+};
 
 export default {
   App,
-}
+};
