9c6f8658e525c9a9877145a732e2ffc3d9e3262e
aubeOSG <caube@osg.ca>
2022-04-11 11:47:21 -0400

    UPDATE [sc-564] - added owlui to course authoring
    - update owlui to latest

--- apps/course-authoring/src/renderer/pages/home/index.tsx
+++ apps/course-authoring/src/renderer/pages/home/index.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import style from './styles.module.scss';
+import { Default as Nav } from '@owlui/navigationdrawer';
+import { Default as Btn } from '@owlui/button';
+import { Default as Icon } from '@owlui/icons';
 
 export const Route = '/';
 export const Name = 'Home';
 
+const Header = (
+  <>
+    <div>
+      <h1 className={style.navTitle}>
+        <Icon className={style.navTitleIcon} icon="school" />
+        <span>Scrowl</span>
+      </h1>
+    </div>
+    <div className={style.navActions}>
+      <div>
+        <Btn size="Sm">New Project</Btn>
+      </div>
+      <div>
+        <Btn size="Sm" appearance="Link">
+          Open
+        </Btn>
+      </div>
+    </div>
+    <div className={style.navDivider} />
+  </>
+);
+
+const items = [
+  {
+    heading: 'RECENT FILES',
+    items: [{ label: 'AODA v1.0.4' }, { label: 'Working at Heights v4.0.3' }],
+  },
+  {
+    heading: 'HELP',
+    items: [
+      { label: 'User Guide' },
+      { label: 'FAQ' },
+      { label: 'Contact Support' },
+    ],
+  },
+];
+
 export const Element = () => {
   return (
-    <div>
-      <h1 data-testid="app__heading">{Name} Page</h1>
+    <div className={style.container}>
+      <Nav className={style.nav} header={Header} items={items} />
     </div>
   );
 };
