4b20ce2e7a1230c6b51079c1e0bcccfebf2fa083
cicerofonsecaosg <cicero@osg.ca>
2022-04-12 15:54:27 -0400

    Update lint settings

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,31 +1,39 @@
-module.exports = {  
+module.exports = {
   env: {
-    "browser": true,
-    "es6": true,
-    "node": true
-  }, 
-  plugins: [
-    "prettier"
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  extends: ["eslint:recommended", 
-    "prettier", 
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/electron",
-    "plugin:import/typescript" ],
-  parser: "@typescript-eslint/parser",
+  plugins: ['prettier', 'react', 'import', 'jsx-a11y'],
+  parser: '@typescript-eslint/parser',
   rules: {
-      "prettier/prettier": "error",
-      "comma-dangle": [
-        "error",
-        {
-          "arrays": "always-multiline",
-          "objects": "always-multiline",
-          "imports": "always-multiline",
-          "exports": "always-multiline",
-          "functions": "never"
-        }
-      ]
-    }
+    'prettier/prettier': 'error',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
