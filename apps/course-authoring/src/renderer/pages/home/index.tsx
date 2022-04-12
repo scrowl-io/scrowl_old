d28bad92eecc1d5ff3a40198d4c47f8042eb18bb
aubeOSG <caube@osg.ca>
2022-04-12 15:34:27 -0400

    UPDATE - hiding icon from SR

--- apps/course-authoring/src/renderer/pages/home/index.tsx
+++ apps/course-authoring/src/renderer/pages/home/index.tsx
@@ -11,7 +11,7 @@ const Header = (
   <>
     <div>
       <h1 className={style.navTitle}>
-        <Icon className={style.navTitleIcon} icon="school" />
+        <Icon className={style.navTitleIcon} icon="school" aria-hidden="true" />
         <span>Scrowl</span>
       </h1>
     </div>
