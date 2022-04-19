4925d04522816f7bb7d6b105b457c4e59b0418b8
cicerofonsecaosg <cicero@osg.ca>
2022-04-19 16:17:59 -0400

    Update workflow and add new tests

--- apps/course-authoring/tests/home.spec.ts
+++ apps/course-authoring/tests/home.spec.ts
@@ -1,22 +1,26 @@
 import { _electron as electron } from 'playwright';
 import { test, expect } from '@playwright/test';
 
-// let appWindow;
-// let electronApp;
+let appWindow;
+let electronApp;
 
-// test.beforeAll(async () => {
-//   electronApp = await electron.launch({ args: ['./dist/main.js'] });
-//   appWindow = await electronApp.firstWindow();
-// });
+test.beforeAll(async () => {
+  electronApp = await electron.launch({ args: ['./dist/main.js'] });
+  appWindow = await electronApp.firstWindow();
+});
 
 test('Should render title bar title element', async () => {
-  const electronApp = await electron.launch({ args: ['./dist/main.js'] });
-  const appWindow = await electronApp.firstWindow();
   const title = await appWindow.locator('.title-bar__title');
 
   await expect(title).toHaveText('Scrowl - Home');
 });
 
-// test.afterAll(async () => {
-//   await electronApp.close();
-// });
+test('Should render icon element', async () => {
+  const title = await appWindow.locator('.nav__title__icon');
+
+  await expect(title).toHaveText('Scrowl - Homes');
+});
+
+test.afterAll(async () => {
+  await electronApp.close();
+});
