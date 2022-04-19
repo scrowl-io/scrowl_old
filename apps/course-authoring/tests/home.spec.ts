dfbb12ae6ff1b330aa53d47443c4eb6a18438a49
Cicero Fonseca <98419841+CiceroFonsecaOSG@users.noreply.github.com>
2022-04-19 14:37:45 -0400

    Reduce testing to test the CI

--- apps/course-authoring/tests/home.spec.ts
+++ apps/course-authoring/tests/home.spec.ts
@@ -1,20 +1,22 @@
 import { _electron as electron } from 'playwright';
 import { test, expect } from '@playwright/test';
 
-let appWindow;
-let electronApp;
+// let appWindow;
+// let electronApp;
 
-test.beforeAll(async () => {
-  electronApp = await electron.launch({ args: ['./dist/main.js'] });
-  appWindow = await electronApp.firstWindow();
-});
+// test.beforeAll(async () => {
+//   electronApp = await electron.launch({ args: ['./dist/main.js'] });
+//   appWindow = await electronApp.firstWindow();
+// });
 
 test('Should render title bar title element', async () => {
+  const electronApp = await electron.launch({ args: ['./dist/main.js'] });
+  const appWindow = await electronApp.firstWindow();
   const title = await appWindow.locator('.title-bar__title');
 
   await expect(title).toHaveText('Scrowl - Home');
 });
 
-test.afterAll(async () => {
-  await electronApp.close();
-});
+// test.afterAll(async () => {
+//   await electronApp.close();
+// });
