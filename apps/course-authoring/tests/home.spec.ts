55e07311b9f862b942038a5ee1112c8ba09e41ab
cicerofonsecaosg <cicero@osg.ca>
2022-04-19 12:21:49 -0400

    Add example test

---
+++ apps/course-authoring/tests/home.spec.ts
@@ -0,0 +1,20 @@
+import { _electron as electron } from 'playwright';
+import { test, expect } from '@playwright/test';
+
+let appWindow;
+let electronApp;
+
+test.beforeAll(async () => {
+  electronApp = await electron.launch({ args: ['./dist/main.js'] });
+  appWindow = await electronApp.firstWindow();
+});
+
+test('Should render title bar title element', async () => {
+  const title = await appWindow.locator('.title-bar__title');
+
+  await expect(title).toHaveText('Scrowl - Home');
+});
+
+test.afterAll(async () => {
+  await electronApp.close();
+});
