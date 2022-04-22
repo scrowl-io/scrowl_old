01c68d757d22157964998fbbb4e959e1a96fb8eb
aubeOSG <caube@osg.ca>
2022-04-22 15:23:12 -0400

    UPDATE [sc-631] - standardizing config implementations

--- apps/app-downloads/.prettierrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  ...require('../../packages/config/linters/.prettierrc'),
-};
--- packages/config/linters/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  arrowParens: 'avoid',
-  useTabs: false,
-  tabWidth: 2,
-  trailingComma: 'es5',
-};
