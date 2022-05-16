9c6901fb3ca5477839562bdf7767dc9aeb17d84c
cicerofonsecaosg <cicero@osg.ca>
2022-05-16 14:22:05 -0400

    [sc-787] - Update create course card button link

--- apps/course-authoring/src/renderer/pages/home/data.tsx
+++ apps/course-authoring/src/renderer/pages/home/data.tsx
@@ -1,5 +1,5 @@
 import { CardProps } from '../../components/cardgrid/index.types';
-import { PageEditorRoute as StructureRoute } from '../editor/structure';
+import { PageRoutes } from '../editor/routes';
 
 export const sidebarItems = [
   {
@@ -22,7 +22,7 @@ export const cards: CardProps[] = [
     title: 'New Blank Document',
     content: 'Start from scratch with no template applied.',
     btnTitle: 'Create New',
-    btnRoute: StructureRoute,
+    btnRoute: PageRoutes['structure'],
   },
   {
     id: 2,
