9fccd8e2c3748c17ba7a86e2abf0441ea1bdae06
cicerofonsecaosg <cicero@osg.ca>
2022-06-14 15:31:23 -0400

    sc-943 - Move logic from handler to model

--- apps/course-authoring/src/main/services/internal-storage/handlers/preferences/index.tsx
+++ apps/course-authoring/src/main/services/internal-storage/handlers/preferences/index.tsx
@@ -1,38 +1,24 @@
-import { ipcMain } from 'electron';
 import database from '../../database';
 import { Preferences } from './index.types';
 
-const table = 'preference';
+const table = 'preferences';
 
 const getPreferencesList = async () => {
   const data = await database.select().from(table);
   return data[0];
 };
 
-const getPreference = async (
-  event: Electron.IpcMainInvokeEvent,
-  args: keyof Preferences
-) => {
-  const data = await database.select(args).from(table);
+const getPreference = async (column: keyof Preferences) => {
+  const data = await database.select(column).from(table);
   return data[0];
 };
 
-const setPreferences = (event: Electron.IpcMainInvokeEvent, args: unknown) => {
+const setPreferences = (args: unknown) => {
   return database(table).update(args);
 };
 
-export const events = {
-  getPreferencesList: 'get-preferences-list',
-  getPreference: 'get-preference',
-  setPreferences: 'set-preferences',
-};
-
-export const init = () => {
-  ipcMain.handle(events.getPreferencesList, getPreferencesList);
-  ipcMain.handle(events.getPreference, (event, args) =>
-    getPreference(event, args)
-  );
-  ipcMain.handle(events.setPreferences, (event, args) =>
-    setPreferences(event, args)
-  );
+export default {
+  getPreferencesList,
+  getPreference,
+  setPreferences,
 };
