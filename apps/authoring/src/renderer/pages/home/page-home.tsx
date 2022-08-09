b37dc70bce85a2292b5e2f36a346ff917e5e9189
Valerie Wireko Osei <valeriew@SML391TO.local>
2022-08-09 10:31:15 -0400

    initial commit of updates to home screen

--- apps/authoring/src/renderer/pages/home/page-home.tsx
+++ apps/authoring/src/renderer/pages/home/page-home.tsx
@@ -10,6 +10,7 @@ export const PageElement = () => {
   project.ready();
 
   const isProcessing = project.useProcessing();
+  // const recentProjects = [];
 
   return (
     <>
@@ -17,6 +18,38 @@ export const PageElement = () => {
       <main className={styles.main}>
         <div>{isProcessing ? <div>WORKING ON IT</div> : ''}</div>
         <h1>Home Page</h1>
+        <h2>SCROWL</h2>
+        <section className="section-row">
+          <div>
+            <h3>Start</h3>
+            <ul>
+              <li>
+                <a href="">New Project</a>
+              </li>
+              <li>
+                <a href="">Open</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Getting Started</h3>
+            <ul>
+              <li>
+                <a href="">Beginner Tutorial Project</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="section-row">
+          <div>
+            <h3>Recent</h3>
+            <ul>
+              {/* {recentProjects.map(recentProject => {
+                return <li></li>;
+              })} */}
+            </ul>
+          </div>
+        </section>
       </main>
     </>
   );
