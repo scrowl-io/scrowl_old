e82ead4dfb5847089d29bf9e4f80af511db6f1f0
aubeOSG <caube@osg.ca>
2022-08-19 13:54:12 -0400

    UPDATE - added filename to toolbar

--- apps/authoring/src/renderer/pages/editor/elements/header/editor-header.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/header/editor-header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import * as styles from './editor-header.module.scss';
 import { Logo, Toolbar } from '../../../../components';
 
 export const Header = () => {
   return (
     <Toolbar>
       <Logo />
+      <div className={styles.filename} data-value="MyCourseProject">
+        <input
+          name="filename"
+          id="filenameInput"
+          className="form-control"
+          value="MyCourseProject"
+          placeholder="Untitled Project"
+        />
+      </div>
     </Toolbar>
   );
 };
