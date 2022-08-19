dd1361fd54c5334ded381ba7b3cf871f8bae43e8
aubeOSG <caube@osg.ca>
2022-08-19 16:21:45 -0400

    UPDATE - filename editable

--- apps/authoring/src/renderer/pages/editor/elements/header/editor-header.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/header/editor-header.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styles from './editor-header.module.scss';
 import { Logo, Toolbar } from '../../../../components';
 
 export const Header = () => {
+  const [filename, setFilename] = useState('MyCourseProject');
+
+  const handleFilenameChange = (ev: React.FormEvent<HTMLInputElement>) => {
+    setFilename(ev.currentTarget.value);
+  };
+
   return (
     <Toolbar>
       <Logo />
-      <div className={styles.filename} data-value="MyCourseProject">
+      <div className={styles.filename} data-value={filename}>
         <input
           name="filename"
           id="filenameInput"
           className="form-control"
-          value="MyCourseProject"
+          value={filename}
           placeholder="Untitled Project"
+          onChange={handleFilenameChange}
         />
       </div>
     </Toolbar>
