9a6d9d1386c20fec2ace0cffa281aa89c5276c06
aubeOSG <caube@osg.ca>
2022-08-19 06:50:56 -0400

    UPDATE - rough styling for glossary

--- apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/editor-pane-details-glossary.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/editor-pane-details-glossary.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import * as styles from '../editor-pane-details.module.scss';
+import { glossaryData } from './mock-data';
+
+export type GlossaryItem = { name: string; description: string };
+export type GlossaryData = Array<GlossaryItem>;
+export type GlossaryDict = {
+  [key: string]: {
+    [key: string]: string;
+  };
+};
+
+const createGlossaryDict = (data: GlossaryData) => {
+  const glossary: GlossaryDict = {};
+
+  data.forEach((item: GlossaryItem) => {
+    const firstLetter = item.name.substring(0, 1);
+
+    if (glossary[firstLetter] === undefined) {
+      glossary[firstLetter] = {};
+    }
+
+    glossary[firstLetter][item.name] = item.description;
+  });
+
+  return glossary;
+};
+
+const createGlossaryItems = (data: GlossaryDict) => {
+  const headings = Object.keys(data).sort();
+
+  return headings.map((heading: string, idxH: number) => {
+    const entries = Object.keys(data[heading]).sort();
+    const glossaryItemElements = entries.map((entry, idxE) => {
+      return (
+        <div className={styles.tabGlossaryTerm} key={idxE}>
+          <dt className={styles.tabGlossaryTermWord}>{entry}</dt>
+          <dd className={styles.tabGlossaryTermDefinition}>
+            {data[heading][entry]}
+          </dd>
+        </div>
+      );
+    });
+    return (
+      <div key={idxH}>
+        <header>{heading}</header>
+        {glossaryItemElements}
+      </div>
+    );
+  });
+};
 
 export const TabGlossary = () => {
-  return <div>Resources Tab</div>;
+  const glossaryDict = createGlossaryDict(glossaryData);
+  const glossaryItems = createGlossaryItems(glossaryDict);
+
+  return (
+    <div className={styles.tabGlossary}>
+      <dl className={styles.tabGlossaryList}>{glossaryItems}</dl>
+    </div>
+  );
 };
 
 export default {
--- apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/mock-data.ts
+++ apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/mock-data.ts
@@ -1,37 +1,37 @@
-export const glossaryItems = [
+export const glossaryData = [
   {
     name: 'Agent',
-    content:
+    description:
       'One who acts for, or in the place of, another, by authority from him or her; one entrusted with the business of another; a substitute; a deputy. Managers and supervisors are agents of the employer.',
   },
   {
     name: 'Circuit courts',
-    content:
+    description:
       'The name informally used to refer to the existing U.S. court of appeals, which are organized into thirteen circuits covering different geographical areas of the country. The term derives from an age before mechanized transit, when judges and lawyers rode “the circuit” of their territory to hold court in various places.',
   },
   {
     name: 'Common Law Torts',
-    content:
+    description:
       'Legal actions against civil wrongs, including assault and battery, intentional infliction of emotional distress, interference with contract and defamation. Tort actions may provide more relief than the federal and state laws. Constructive Discharge.',
   },
   {
     name: 'Discrimination',
-    content:
+    description:
       'Any action that unlawfully or unjustly results in unequal treatment of persons or groups based on race, color, gender, national origin, religion, age, disability or other factors protected under federal, state or local laws, such as marital status or gender identity.',
   },
   {
     name: 'Coercion',
-    content:
+    description:
       'The use of authority or force to impose an unwanted advance. The act of compelling by force of authority.',
   },
   {
     name: 'Assault',
-    content:
+    description:
       'Assault can be defined as any act in which a person is abused, threatened, intimidated or assaulted in his or her employment. While exact definitions vary in legislation, generally speaking workplace violence or harassment includes: Threatening behaviour – such as shaking fists, destroying property or throwing objects.',
   },
   {
     name: 'Bullying',
-    content:
+    description:
       "Objectionable behavior in the form of repeated and hostile/unwanted conduct, verbal comments, actions, or gestures. This behavior affects an employee's dignity or pychological or physical integrity, serves no legitimate work-related purpose, and results in a harmful work environment for the employee.",
   },
 ];
@@ -48,6 +48,6 @@ export const resourceData = [
 ];
 
 export default {
-  glossaryItems,
+  glossaryData,
   resourceData,
 };
