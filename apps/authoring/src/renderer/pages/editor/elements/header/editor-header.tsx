910c4c22029a85caebe20632f5bd0c068ccd81fb
cicerofonsecaosg <cicero@osg.ca>
2022-08-25 20:21:15 -0400

    sc-1293 Add preview button and update save hint icon

--- apps/authoring/src/renderer/pages/editor/elements/header/editor-header.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/header/editor-header.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import * as styles from './editor-header.module.scss';
+import * as toolbarStyles from '../../../../components/toolbar/comp-toolbar.module.scss';
 import { Logo, Toolbar } from '../../../../components';
+import { PreviewButton } from './elements';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 export const Header = () => {
   const [filename, setFilename] = useState('MyCourseProject');
@@ -20,7 +23,35 @@ export const Header = () => {
           value={filename}
           placeholder="Untitled Project"
           onChange={handleFilenameChange}
+          size={2}
         />
+      </div>
+      <div className={`collapse ${toolbarStyles.toolbarCollapse}`}>
+        <ul
+          className={`${toolbarStyles.toolbarNav} align-items-center me-auto`}
+        >
+          <li className="nav-item">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Unsaved Changes</Tooltip>}
+              delay={0}
+            >
+              <div
+                className="btn btn-save-state"
+                role="button"
+                aria-pressed="false"
+                tabIndex={0}
+              >
+                <span className="save-state">*</span>
+              </div>
+            </OverlayTrigger>
+          </li>
+        </ul>
+        <ul className={`${toolbarStyles.toolbarNav} align-items-center`}>
+          <li className="scrowl-navbar__actions">
+            <PreviewButton />
+          </li>
+        </ul>
       </div>
     </Toolbar>
   );
