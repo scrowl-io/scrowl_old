84525d0a5ae4da4f8a3779c582b0de9c4a380aef
aubeOSG <caube@osg.ca>
2022-08-31 20:30:49 -0400

    UPDATE - added example of a lesson update

--- apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/tree-view/editor-tree-view-lessons.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/tree-view/editor-tree-view-lessons.tsx
@@ -2,24 +2,40 @@ import React, { useState } from 'react';
 import * as styles from '../../editor-pane-details.module.scss';
 import { Icon, Button } from '@owlui/lib';
 import Collapse from 'react-bootstrap/Collapse';
+import { Projects } from '../../../../../../models';
 import { ActionMenu, ActionMenuItem } from '../../../../../../components';
 import {
+  ModuleTreeItem,
+  LessonTreeItem,
+  SlideTreeItem,
   TreeViewLessonsProps,
   TreeViewLessonProps,
 } from './editor-tree-view.types';
 import { TreeViewSlides } from './editor-tree-view-slides';
+import { deepCopy } from './utils';
 
 const TreeViewLesson = (props: TreeViewLessonProps) => {
   const { tree, idx, moduleIdx, project } = props;
   const [open, setOpen] = useState(false);
   const itemId = `module-${moduleIdx}-lesson-item-${idx}`;
   const menuId = `module-${moduleIdx}-lesson-menu-${idx}`;
+  const modules = deepCopy(project.modules);
+  const lessonModule: ModuleTreeItem = modules[moduleIdx];
+  const lesson: LessonTreeItem = lessonModule.lessons[idx];
 
   const lessonMenuItems: Array<ActionMenuItem> = [
     {
       label: 'Add Slide',
       icon: 'crop_square',
       iconStyle: 'Outlined',
+      action: () => {
+        const newSlide: SlideTreeItem = {
+          name: 'Umtitled Slide',
+        };
+
+        lesson.slides.push(newSlide);
+        Projects.update({ modules });
+      },
     },
   ];
 
