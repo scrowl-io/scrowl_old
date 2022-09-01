125647b32ed8f91e3c0e08d15ddf2c7924e2f0f6
aubeOSG <caube@osg.ca>
2022-08-31 20:39:40 -0400

    UPDATE - added example of updating a slide

--- apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/tree-view/editor-tree-view-slides.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/pane-details/elements/tree-view/editor-tree-view-slides.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import * as styles from '../../editor-pane-details.module.scss';
 import { Icon, Button } from '@owlui/lib';
+import { Projects } from '../../../../../../models';
 import { ActionMenu, ActionMenuItem } from '../../../../../../components';
 import {
+  ModuleTreeItem,
+  LessonTreeItem,
+  SlideTreeItem,
   TreeViewSlidesProps,
   TreeViewSlideProps,
 } from './editor-tree-view.types';
+import { deepCopy, addLeadZero } from './utils';
 
 const TreeViewSlide = (props: TreeViewSlideProps) => {
   const { tree, idx, moduleIdx, lessonIdx, project } = props;
   const itemId = `module-${moduleIdx}-lesson-${lessonIdx}-slide-item-${idx}`;
+  const modules = deepCopy(project.modules);
+  const slideModule: ModuleTreeItem = modules[moduleIdx];
+  const slideLesson: LessonTreeItem = slideModule.lessons[lessonIdx];
+  const slide: SlideTreeItem = slideLesson.slides[idx];
+
   const slideMenuItems: Array<ActionMenuItem> = [
     {
       label: 'Rename',
       icon: 'edit',
       iconStyle: 'Outlined',
+      action: () => {
+        const now = new Date();
+        const y = now.getFullYear().toString().slice(-2);
+        const m = addLeadZero(now.getMonth() + 1);
+        const d = addLeadZero(now.getDate());
+        const hh = addLeadZero(now.getHours());
+        const mm = addLeadZero(now.getMinutes());
+        const ss = addLeadZero(now.getSeconds());
+
+        slide.name = `${y}/${m}/${d} - ${hh}:${mm}:${ss}`;
+        Projects.update({ modules });
+      },
     },
   ];
 
