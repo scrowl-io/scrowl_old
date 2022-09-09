f5c4915f0b13908a213b78ddb86109f6e9e0d789
aubeOSG <caube@osg.ca>
2022-09-09 05:40:28 -0400

    LINITING

--- apps/authoring/src/renderer/pages/editor/elements/pane-details/editor-pane-details.tsx
+++ apps/authoring/src/renderer/pages/editor/elements/pane-details/editor-pane-details.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as styles from './editor-pane-details.module.scss';
 import { Tabs } from '@owlui/lib';
 import { Pane } from '../../../../components';
 import { TabOutline, TabGlossary, TabResources } from './elements';
