2e36893c649be0a2d2bb7e925d7b19d4e44a4339
Scott <contact@scottvincent.ca>
2022-09-23 13:43:26 -0400

    FIX Styling around action btn dropdown icon sizing.

--- apps/authoring/src/renderer/components/action-menu/comp-action-menu.tsx
+++ apps/authoring/src/renderer/components/action-menu/comp-action-menu.tsx
@@ -11,12 +11,7 @@ import {
 } from '@owlui/lib';
 
 const ActionMenuBtn = (
-  <Icon
-    display="outlined"
-    filled
-    icon="more_vert"
-    style={{ fontSize: '15px', fontWeight: 1000 }}
-  />
+  <Icon display="rounded" icon="more_vert" opsz={20} filled />
 );
 
 const makeActionMenu = (
