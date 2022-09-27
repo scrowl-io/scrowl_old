f7b6e2d07f52d2ba82b88a92bf5d66a4f566f865
aubeOSG <caube@osg.ca>
2022-09-26 23:44:12 -0400

    Update text.tsx

--- apps/authoring/src/renderer/components/formBuilder/elements/text.tsx
+++ apps/authoring/src/renderer/components/formBuilder/elements/text.tsx
@@ -4,7 +4,6 @@ import { FormElementProps, FormBuilderData } from '../comp-form-builder.types';
 import { deepCopy } from '../utils';
 
 export const Text = ({ config, name, onUpdate }: FormElementProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const payload: { [key: string]: FormBuilderData } = {};
   const control = deepCopy(config);
   const [value, setValue] = useState(config.value);
