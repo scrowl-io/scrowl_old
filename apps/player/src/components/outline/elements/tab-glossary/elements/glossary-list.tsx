0be31b22f9e223d3c08f9512b3e7b4c1969a7b9d
Valerie Wireko Osei <valeriew@SML391TO.local>
2022-09-28 09:42:41 -0400

    brought read-only glossary into player left pane

---
+++ apps/player/src/components/outline/elements/tab-glossary/elements/glossary-list.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  GlossaryListProps,
+  GlossaryItem,
+  GlossaryData,
+  GlossaryDict,
+} from '../../../player-outline.types';
+import * as styles from '../../../player-outline.module.scss';
+
+export const GlossaryListEntries = ({ glossary }: GlossaryListProps) => {
+  const entries = Object.keys(glossary).sort((a, b) => {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  });
+
+  return (
+    <>
+      {entries.map((entry: string, idx: number) => {
+        return (
+          <div
+            className={styles.tabGlossaryTerm}
+            key={idx}
+            id={`glossary-item-${entry}`}
+          >
+            <div className="d-flex justify-content-between">
+              <dt className={styles.tabGlossaryTermWord}>{entry}</dt>
+            </div>
+            <dd className={styles.tabGlossaryTermDefinition}>
+              {glossary[entry].description}
+            </dd>
+          </div>
+        );
+      })}
+      ;
+    </>
+  );
+};
+
+export const GlossaryList = ({ glossary }: GlossaryListProps) => {
+  const headings = Object.keys(glossary).sort();
+
+  console.log('headings', headings);
+  if (!headings) {
+    return <></>;
+  }
+
+  return (
+    <>
+      {headings.map((heading, idx: number) => {
+        return (
+          <div key={idx}>
+            <header className={styles.tabGlossaryHeader}>{heading}</header>
+            <GlossaryListEntries glossary={glossary[heading]} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default {
+  GlossaryList,
+  GlossaryListEntries,
+};
