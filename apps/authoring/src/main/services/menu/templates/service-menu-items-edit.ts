3036b410ec3b9612431672a44bb2ef5338946ee9
gregterleckiOSG <gterlecki@osg.ca>
2022-09-29 10:23:23 -0400

    Edit menu, including copy and paste..

---
+++ apps/authoring/src/main/services/menu/templates/service-menu-items-edit.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MenuItemConstructorOptions } from 'electron';
+
+const separator: MenuItemConstructorOptions = { type: 'separator' };
+
+export const template: MenuItemConstructorOptions = {
+  label: 'Edit',
+  submenu: [
+    {
+      label: 'Copy',
+      role: 'copy',
+      accelerator: 'CmdOrCtrl+C',
+    },
+    {
+      label: 'Paste',
+      role: 'paste',
+      accelerator: 'CmdOrCtrl+V',
+    },
+    {
+      label: 'Undo',
+      role: 'undo',
+      accelerator: 'CmdOrCtrl+Z',
+    },
+    {
+      label: 'Redo',
+      role: 'redo',
+      accelerator: 'CmdOrCtrl+Y',
+    },
+  ],
+};
+
+export default {
+  template,
+};
